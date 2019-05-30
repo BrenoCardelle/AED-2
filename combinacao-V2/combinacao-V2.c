10993892c585d6da125097a3436791fe983e7efa
BrenoCardelle <43342547+BrenoCardelle@users.noreply.github.com>
2019-05-30 11:40:46 -0300

    Update combinacao-V2.c

--- combinacao-V2/combinacao-V2.c
+++ combinacao-V2/combinacao-V2.c
@@ -3,6 +3,7 @@
 int np = 0;
 int P[4] = {0,0,0,0};
 int q,n;
+int cont=0;
 
 void Comb(int t){
     int i;
@@ -10,8 +11,11 @@ void Comb(int t){
         np = np + 1;
         P[np] = i;
         if(np == q){
-            printf("%d,%d,%d \n", P[1],P[2],P[3]);}
-        else{Comb(i+1);}
+            if(cont <=50){
+                cont+=1;
+                printf("%d,%d,%d \n", P[1],P[2],P[3]);
+            }
+        }else{Comb(i+1);}
         np = np - 1;
     }
 }
