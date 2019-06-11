ad3e2ca14fc0a738812baabc6bde8c92342752e7
BrenoCardelle <43342547+BrenoCardelle@users.noreply.github.com>
2019-06-11 12:00:55 -0300

    Update 4_quadrados.c

--- soma_dos_4_quadrados/4_quadrados.c
+++ soma_dos_4_quadrados/4_quadrados.c
@@ -1,22 +1,25 @@
 #include <stdio.h>
 #include <math.h>
 
-int V[5] = {0,0,0,0,0};
+int V[7] = {0,0,0,0,0,0,0};
 int i;
 int np=0;
 void Quadrado(int n){
     for(i=1; i<=4; i++){
         if(pow(V[1],2) + pow(V[2],2) + pow(V[3],2) + pow(V[4],2) < n){
-            np+=1;
+            np=np+1;
             V[np]=i;
             if(pow(V[1],2) + pow(V[2],2) + pow(V[3],2) + pow(V[4],2) == n){
             	printf("%d %d %d %d \n", V[4], V[3], V[2], V[1]);
 			}else{
             	Quadrado(n);
             }
-            V[np]=0;
-            np-=1;
-        }
+            np=np-1;
+            int j;
+            for(j = 1; j<=7; j++){
+            	V[j]=0;
+			}
+        }else if(pow(V[1],2) + pow(V[2],2) + pow(V[3],2) + pow(V[4],2) == n){printf("%d %d %d %d \n", V[4], V[3], V[2], V[1]);}
     }
 }
 
